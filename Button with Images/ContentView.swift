cfe9e86a3cff41fdd76e17fde3671dfd0de31796
Hans Lehman <hans.lehman@gmail.com>
2022-02-14 12:09:21 -0500

    Trying to figure out why the deck of 52 cards is not being created.

--- Button with Images/ContentView.swift
+++ Button with Images/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    //variables go here
+    @State private var deckOfCards: [Image]=[]
+    @State private var suits: [String]=["_of_diamonds","_of_spades","_of_clubs","_of_hearts"]
+    @State private var cardNames: [String]=["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
+    func createDeck() {
+        // used to use this
+    }
+    
+   
+    init(){
+       // createDeck()
+        
+        print("There are\(deckOfCards.count) cards")
+        print("There are\(suits.count) suits")
+        print("There are\(cardNames.count) cards")
+        for card in cardNames{
+            for suit in suits {
+                deckOfCards.append(Image("\(card)\(suit)"))
+            }
+        }
+        print("There are\(deckOfCards.count) cards")
+       
+    }
+   
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        VStack{
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world2!")
+                .padding()
+            Button(action: {
+                deckOfCards.append(deckOfCards[0])
+                deckOfCards.remove(at: 0)
+            //label: deckOfCards[0]
+                
+                //code goes here
+            }, label: {deckOfCards[0]
+            })
+            //Image:
+        }
+        
+        
+        
+        
+        
+        
     }
 }
 
