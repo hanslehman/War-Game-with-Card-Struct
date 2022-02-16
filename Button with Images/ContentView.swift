4550c2b52bfb2387e97bbef4ad99bfa6cfd5f39a
Hans Lehman <hans.lehman@gmail.com>
2022-02-16 10:55:28 -0500

    update changes for a working button that flips through and array of images.  The images are a deck of cards.

--- Button with Images/ContentView.swift
+++ Button with Images/ContentView.swift
@@ -7,34 +7,27 @@
 
 import SwiftUI
 
-
-
-
-struct ContentView: View {
-    //variables go here
-    @State private var deckOfCards: [Image]=[]
-    @State private var suits: [String]=["_of_diamonds","_of_spades","_of_clubs","_of_hearts"]
-    @State private var cardNames: [String]=["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
-    func createDeck() {
-        // used to use this
-    }
-    
-   
-    init(){
-       // createDeck()
+class DoOperations: NSObject, ObservableObject{
+    @Published var deckOfCards:[Image]=[]
+    let suits: [String]=["_of_diamonds","_of_spades","_of_clubs","_of_hearts"]
+    let cardNames: [String]=["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
+    override init(){
         
-        print("There are\(deckOfCards.count) cards")
-        print("There are\(suits.count) suits")
-        print("There are\(cardNames.count) cards")
+        super.init()
         for card in cardNames{
             for suit in suits {
                 deckOfCards.append(Image("\(card)\(suit)"))
             }
+            
         }
-        print("There are\(deckOfCards.count) cards")
-       
+        
     }
-   
+}
+
+struct ContentView: View {
+    //variables go here
+    @StateObject var op = DoOperations()
+
     
     var body: some View {
        
@@ -43,15 +36,13 @@ struct ContentView: View {
                 .padding()
             Text("Hello, world2!")
                 .padding()
-            Button(action: {
-                deckOfCards.append(deckOfCards[0])
-                deckOfCards.remove(at: 0)
-            //label: deckOfCards[0]
+            op.deckOfCards[0].imageScale(.large)
+            
+            Button{
+                op.deckOfCards.append(op.deckOfCards[0])
+                op.deckOfCards.remove(at: 0)
                 
-                //code goes here
-            }, label: {deckOfCards[0]
-            })
-            //Image:
+            } label:{ Text("Flip Images")
         }
         
         
@@ -66,4 +57,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
